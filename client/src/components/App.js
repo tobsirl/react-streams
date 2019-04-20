f3c27806848ff57edc39580defd09ea31be3dfdc
Paul Tobin <tobster111@gmail.com>
2019-04-20 13:45:45 +0100

    Added a Header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,23 +2,27 @@ import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 // import components
-import StreamCreate from '../components/streams/StreamCreate';
-import StreamDelete from '../components/streams/StreamDelete';
-import StreamEdit from '../components/streams/StreamEdit';
-import StreamList from '../components/streams/StreamList';
-import StreamShow from '../components/streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/new" component={StreamCreate} />
-        <Route path="/edit" component={StreamEdit} />
-        <Route path="/delete" component={StreamDelete} />
-        <Route path="/show" component={StreamShow} />
-      </div>
-    </BrowserRouter>
+    <div className="ui container">
+      <Header />
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/new" component={StreamCreate} />
+          <Route path="/edit" component={StreamEdit} />
+          <Route path="/delete" component={StreamDelete} />
+          <Route path="/show" component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
 
