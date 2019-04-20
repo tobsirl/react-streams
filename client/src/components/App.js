69fb6efb0380d2fa155ab5ac10a3f18d68d80242
Paul Tobin <tobster111@gmail.com>
2019-04-20 13:35:09 +0100

    Imported the components to App and setup routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
+// import components
+import StreamCreate from '../components/streams/StreamCreate';
+import StreamDelete from '../components/streams/StreamDelete';
+import StreamEdit from '../components/streams/StreamEdit';
+import StreamList from '../components/streams/StreamList';
+import StreamShow from '../components/streams/StreamShow';
+
 const App = () => {
   return (
     <BrowserRouter>
       <div>
-        <h1>APP</h1>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/new" component={StreamCreate} />
+        <Route path="/edit" component={StreamEdit} />
+        <Route path="/delete" component={StreamDelete} />
+        <Route path="/show" component={StreamShow} />
       </div>
     </BrowserRouter>
   );
