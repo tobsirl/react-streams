81477a225c5727b4e64c43d2793128d7cabce6e8
Paul Tobin <tobster111@gmail.com>
2019-04-20 13:29:23 +0100

    Added all the streaming components with basic template

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h1>APP</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>APP</h1>
+      </div>
+    </BrowserRouter>
   );
 };
 
