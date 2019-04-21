1886c4503fbfb7407ec67c8e1d91fbf85f544bf4
Paul Tobin <tobster111@gmail.com>
2019-04-21 11:58:20 +0100

    Added a GoogleAuth component with componentDidMount calling the google api

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId:
+          '139946060856-ja20bf7qskgf03gb4bepkum7rrb6l07p.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// import components
+import GoogleAuth from './GoogleAuth';
+
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">
@@ -11,6 +14,7 @@ const Header = () => {
       <Link to="/" className="item">
         All Streams
       </Link>
+      <GoogleAuth />
     </div>
   );
 };
