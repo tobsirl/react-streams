132a724ce529e38a25213394ba266e3060478e6b
Paul Tobin <tobster111@gmail.com>
2019-04-23 16:52:55 +0100

    Add formReducer to reducers/indes.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 // setup a dummy reducer to get the app running
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
