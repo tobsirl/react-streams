d1a9ae729430f80633fa82523d0040b5ec7fb1b5
Paul Tobin <tobster111@gmail.com>
2019-04-25 17:03:20 +0100

    Finished out createStream with dispatch type and payload

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -16,5 +16,7 @@ export const signOut = () => {
 
 //Using react-thunk
 export const createStream = formValues => async dispatch => {
-  streams.post('/streams', formValues);
+  const response = await streams.post('/streams', formValues);
+
+  dispatch({ type: CREATE_STREAM, payload: response.data });
 };
