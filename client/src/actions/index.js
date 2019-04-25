695d0d55a4505d2fb08e312317df8fb246c17b27
Paul Tobin <tobster111@gmail.com>
2019-04-25 17:14:38 +0100

    Added an action for fetchStreams were all the streams are returned to the user

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/steams';
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from './types';
 
 export const signIn = userId => {
   return {
@@ -15,11 +23,20 @@ export const signOut = () => {
 };
 
 //Using react-thunk
-// Create a stream 
+// Create a stream
 export const createStream = formValues => async dispatch => {
   // send data to the backend server
   const response = await streams.post('/streams', formValues);
 
   // update the store
   dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+// Fetch all the steams
+export const fetchStreams = () => async dispatch => {
+  // get list of stream from the api server
+  const response = await streams.get('/streams');
+
+  // update the store with the list of streams
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
