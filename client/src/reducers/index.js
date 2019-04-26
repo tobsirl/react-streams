e315be9e091228da8ff58070d52ba70a984fa7d9
Paul Tobin <tobster111@gmail.com>
2019-04-26 17:55:47 +0100

    Added stream reducer to the index.js file

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form'; // remame to formReducer
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 // setup a dummy reducer to get the app running
 export default combineReducers({
   auth: authReducer,
-  form: formReducer // must be named form for the redux-form package
+  form: formReducer, // must be named form for the redux-form package
+  streams: streamReducer
 });
