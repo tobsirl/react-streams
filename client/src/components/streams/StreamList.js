9e21befecb8a7e844aba77d2a74044c6c078f492
Paul Tobin <tobster111@gmail.com>
2019-04-26 19:29:23 +0100

    Added comments and material ui styling

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -5,17 +5,37 @@ import { fetchStreams } from '../../actions';
 
 class StreamList extends Component {
   componentDidMount() {
-    this.props.fetchStreams();
+    this.props.fetchStreams(); // call the fetchStreams action
+  }
+
+  // create a method to map over the incoming data
+  renderStreams() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
   }
 
   render() {
-    return <div>StreamList</div>;
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderStreams()}</div>
+      </div>
+    );
   }
 }
 
-const mapStateToProps = state => ({
-  streams: state.streams
-});
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams) }; // changed from Object to an array
+};
 
 export default connect(
   mapStateToProps,
