63858a4599b0c95ef63b7f5bb55ebcc143f8a70d
Paul Tobin <tobster111@gmail.com>
2019-04-26 17:53:28 +0100

    Installed lodash and use omit and mapKeys for DELETE_STREAM and FETCH_STREAMS

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -6,18 +6,20 @@ import {
   EDIT_STREAM
 } from '../actions/types';
 
+import _ from 'lodash';
+
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
-    case FETCH_STREAMS:
-      return;
     case DELETE_STREAM:
-      return;
+      return _.omit(state, action.payload);
     default:
       return state;
   }
