6b04c6c2017cc943c7ad2ba83b11f6238c3ae026
Paul Tobin <tobster111@gmail.com>
2019-04-28 17:15:26 +0100

    Change BrowserRouter to Router in the App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 
 // import components
 import StreamCreate from './streams/StreamCreate';
@@ -8,11 +8,12 @@ import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <div>
           <Header />
           <Route path="/" exact component={StreamList} />
@@ -21,7 +22,7 @@ const App = () => {
           <Route path="/delete" component={StreamDelete} />
           <Route path="/show" component={StreamShow} />
         </div>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
