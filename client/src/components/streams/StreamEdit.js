e7bcedc2988074107e8dd6cba5a6b1ab9876dba7
Paul Tobin <tobster111@gmail.com>
2019-04-28 20:06:28 +0100

    Refactored StreamEdit to a class based component, using componentDidMount

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchStream } from '../../actions';
 
-const StreamEdit = props => {
-  console.log(props);
-  return <div>StreamEdit</div>;
-};
+class StreamEdit extends Component {
+  
+
+  render() {
+    return <div>StreamEdit</div>;
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
