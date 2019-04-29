98b9432753675c35cb448c890f7aa6255727fab4
Paul Tobin <tobster111@gmail.com>
2019-04-29 10:31:05 +0100

    Add a StreamForm component to abstract the form inputs

---
+++ client/src/components/streams/StreamForm.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
+
+class StreamForm extends Component {
+  renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  // render the inputs
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''} `;
+    return (
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} />
+        {this.renderError(meta)}
+      </div>
+    );
+  };
+
+  // handle submissions
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field
+          name="title"
+          type="text"
+          component={this.renderInput}
+          label="Enter Title"
+        />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+
+  if (!formValues.title) {
+    // only run if the user did not enter a title
+    errors.title = 'You must enter a title';
+  }
+
+  if (!formValues.description) {
+    // only run if the user did not enter a description
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
+};
+
+const formWrapped = reduxForm({
+  form: 'StreamCreate',
+  validate
+})(StreamForm);
+
+export default connect(
+  null,
+  { createStream }
+)(formWrapped);
