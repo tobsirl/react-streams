881a9c366742c731f5a6e039f0ea10bddd9e2e5b
Paul Tobin <tobster111@gmail.com>
2019-04-30 13:36:39 +0100

    Updated the Link in StreamList

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -13,10 +13,10 @@ class StreamList extends Component {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <Link to={`/edit/${stream.id}`} className="ui button primary">
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
             Edit
           </Link>
-          <button className="ui button negative">Delete</button>
+          <Link to={`/streams/delete/${stream.id}`} className="ui button negative">Delete</Link>
         </div>
       );
     }
@@ -42,7 +42,7 @@ class StreamList extends Component {
     if (this.props.isSignedIn) {
       return (
         <div style={{ textAlign: 'right' }}>
-          <Link to="/new" className="ui button primary">
+          <Link to="/streams/new" className="ui button primary">
             Create Stream
           </Link>
         </div>
