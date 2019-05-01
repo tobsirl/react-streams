32163a699de9221a9b5d1e031f251b5c0f32be37
Paul Tobin <tobster111@gmail.com>
2019-05-01 15:01:41 +0100

    Using Switch from react-router-dom to make a exact match on the url

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 // import components
 import StreamCreate from './streams/StreamCreate';
@@ -16,11 +16,13 @@ const App = () => {
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit/:id" component={StreamEdit} />
-          <Route path="/streams/delete/:id" component={StreamDelete} />
-          <Route path="/streams/show/:id" component={StreamShow} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/show/:id" exact component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
